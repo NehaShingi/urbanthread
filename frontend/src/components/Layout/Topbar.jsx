cee6bab18727a29114521bcea95e523f1a5a0df5
NehaShingi <shingineha836@gmail.com>
2026-07-16 00:06:58 +0530

    Implement footer section

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -6,7 +6,7 @@ const Topbar = () => {
   return (
     <div className="bg-red-600 text-white">
       <div className="container mx-auto flex justify-between items-center py-3 px-4">
-        <div className="hidden md:flex items-center space-x-4">
+        <div className=" hidden md:flex flex-1 items-center space-x-4">
           {/* 1st icon */}
           <a href="#" className="hover:text-gray-300">
             <TbBrandMeta className="h-5 w-5" />
@@ -20,11 +20,13 @@ const Topbar = () => {
             <RiTwitterXLine className="h-4 w-4" />
           </a>
         </div>
-        <div className="text-sm text-center flex-grow">
-          <span>We ship world-wide- Fast and reliable shipping</span>
+        <div className="flex-1 text-sm text-center ">
+          <span className="ml-2">
+            We ship world-wide- Fast and reliable shipping
+          </span>
         </div>
 
-        <div className="hidden text-sm md:block">
+        <div className="hidden md:flex flex-1 justify-end text-sm">
           <a href="tel:+1234567890" className="hover:text-gray-300">
             +1 (234) 567-890
           </a>
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,10 +1,15 @@
 //It will contain Header, Main Content, Footer
+import Footer from "../Common/Footer.jsx";
 import Header from "../Common/Header.jsx";
 
 const UserLayout = () => {
   return (
     <>
+      {/*Header*/}
       <Header />
+      {/*Main Content*/}
+      {/*Footer*/}
+      <Footer />
     </>
   );
 };

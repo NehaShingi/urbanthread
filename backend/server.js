5e71ee28c58e396651b93d2b1e2df41a7e639137
NehaShingi <shingineha836@gmail.com>
2026-09-03 23:57:00 +0530

    Implement checkout and order routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const checkoutRoutes = require("./routes/checkoutRoutes");
 
 const app = express();
 app.use(express.json());
@@ -13,7 +14,7 @@ app.use(cors());
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-//Connect to MOngoDB Database
+//Connect to MongoDB Database
 connectDB();
 
 app.get("/", (req, res) => res.send("Welcome to UrbanThread"));
@@ -22,6 +23,7 @@ app.get("/", (req, res) => res.send("Welcome to UrbanThread"));
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/checkout", checkoutRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
